46b0ce3ae3ec8f1a705e1ed3940f2c389aa16996
Pranav <pranavjain3194@gmail.com>
2018-05-02 16:01:17 -0400

    feat: added computeCost

--- ex1/computeCost.m
+++ ex1/computeCost.m
@@ -5,13 +5,12 @@ function J = computeCost(X, y, theta)
 
 % Initialize some useful values
 m = length(y); % number of training examples
+yPredicted = X * theta;
+temp = yPredicted - y;
+temp = temp .^ 2;
+J = sum(temp)/(2*m);
 
-% You need to return the following variables correctly 
-J = 0;
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta
-%               You should set J to the cost.
 
 
 
--- ex1/plotData.m
+++ ex1/plotData.m
@@ -6,7 +6,7 @@ function plotData(x, y)
 figure; % open a new figure window
 plot(x,y,'rx','MarkerSize',10);
 ylabel('Profit in $10,000s');
-xlabel('Population of City in $10,000s');
+xlabel('Population of City in 10,000s');
 v = axis;
 extend = [-5 5 -5 5];
 axis(v+extend);
