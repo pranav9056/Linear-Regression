d60687710597f339d9e389fe84442684f3af9c8f
Pranav <pranavjain3194@gmail.com>
2018-05-02 17:37:08 -0400

    added multi files

--- ex1/computeCostMulti.m
+++ ex1/computeCostMulti.m
@@ -5,18 +5,9 @@ function J = computeCostMulti(X, y, theta)
 
 % Initialize some useful values
 m = length(y); % number of training examples
-
-% You need to return the following variables correctly 
-J = 0;
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta
-%               You should set J to the cost.
-
-
-
-
-
-% =========================================================================
+yPredicted = X * theta;
+temp = yPredicted - y;
+temp = temp .^ 2;
+J = sum(temp)/(2*m);
 
 end
--- ex1/gradientDescentMulti.m
+++ ex1/gradientDescentMulti.m
@@ -8,27 +8,11 @@ m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
 
 for iter = 1:num_iters
-
-    % ====================== YOUR CODE HERE ======================
-    % Instructions: Perform a single gradient step on the parameter vector
-    %               theta. 
-    %
-    % Hint: While debugging, it can be useful to print out the values
-    %       of the cost function (computeCostMulti) and gradient here.
-    %
-
-
-
-
-
-
-
-
-
-
-
-    % ============================================================
-
+    yPredicted  = X*theta;
+    predictionDifferece = yPredicted - y;
+    derivative = sum(predictionDifferece .* X)*alpha/m;
+    deltaTheta = derivative';
+    theta = theta - deltaTheta;
     % Save the cost J in every iteration    
     J_history(iter) = computeCostMulti(X, y, theta);
 
